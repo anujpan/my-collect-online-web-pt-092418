3cd579052d0860abcce0e8d9d9016b07db1fb6e2
Anuj Panchal <anujpan1@gmail.com>
2018-10-06 20:31:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,3 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
 def my_collect(arr)
   i = 0
   new_array = []
@@ -9,9 +7,4 @@ def my_collect(arr)
   end
   
   new_array
-end
-
-
-my_collect(array) do |name|
-  name.split(" ").first
 end
